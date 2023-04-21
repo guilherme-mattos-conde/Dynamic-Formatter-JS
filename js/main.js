7a33428c272d177982cba9077c9cc27a2eb424e3
Guilherme Mattos Conde <guimattos205@gmail.com>
2023-04-20 22:17:32 -0300

    Style Commit
    In this commit I made changes in the style and in the JavaScript.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
 const texto = document.querySelector('#texto')
 const input = document.querySelector('#input')
 
-texto.textContent = 'Texto'
+formataTitulo()
 
-input.addEventListener('input', () => {
-
-    texto.textContent = input.value
-
+input.addEventListener('focusin', () => {
     if (input.value === ""){
-        texto.textContent = 'Texto'
+        formataTexto()
     }
 })
+input.addEventListener('focusout', () => {
+    if (input.value === ""){
+        formataTitulo()
+    }
+})
+input.addEventListener('input', () => {
+    texto.textContent = input.value
+    formataTexto()
+})
+
+function formataTitulo(titulo) {
+    texto.textContent = 'tx'
+    texto.classList.remove('texto')
+    texto.classList.add('titulo')
+}
+function formataTexto() {
+    texto.classList.remove('titulo')
+    texto.classList.add('texto')
+    if (input.value === ""){
+        texto.textContent = '[ tx ]'
+    }
+}
